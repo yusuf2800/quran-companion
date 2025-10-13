94be9bfed6e41d1f0a9c5fa5da9cd49ba94eb4db
Yusuf <yusuf@Yusufs-MacBook-Air.local>
2025-10-13 12:51:56 +0100

    fixing

--- src/app/[juz]/quarter/[quarters]/QuranVerse.tsx
+++ src/app/[juz]/quarter/[quarters]/QuranVerse.tsx
@@ -129,7 +129,7 @@ const QuranVerse = ({
 
       <div className="mx-5 mt-24 max-w-screen">
         <div className="px-4 text-white sm:px-8">
-          <div className="my-35 flex items-center justify-center">
+          <div className="my-20 flex items-center justify-center">
             <h1 className="font-quranCommon mx-auto mt-4 text-center text-4xl sm:my-2">
               ﷽
             </h1>
